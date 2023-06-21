30ca8499655e95ba876aad4a75635e5798812f68
Detse233 <ebuatsie@gmail.com>
2023-06-21 13:29:22 -0700

    Print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alpha in lowercase
+ * Return: On success 1
+ */
+void print_alphabet(void)
+{
+	char lowc;
+
+	for (lowc = 'a', lowc <= 'z' lowc++)
+	{
+		_putchar(lowc);
+	}
+	_putchar('\n');
+}
