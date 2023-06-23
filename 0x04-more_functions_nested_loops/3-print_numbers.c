2c20ad170b92107d3643796f78baa9e81af82f91
Detse233 <ebuatsie@gmail.com>
2023-06-23 03:45:14 -0700

    Print integers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int pn;
+
+	for (pn = 48; pn <= 57; pn++)
+	{
+		_putchar(pn);
+	}
+	_putchar('\n');
+}
