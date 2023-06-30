3fc946d57d09e9256b597c76af670b242f02e0a4
Detse233 <ebuatsie@gmail.com>
2023-06-29 18:18:25 -0700

    Concatenate words

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - concatenate words
+ * @dest: input value for
+ * @src: input value in for
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
