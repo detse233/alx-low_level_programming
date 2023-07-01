3d6cdd1a7f60062643bca8b9b3d07d0f3f0af7c3
Detse233 <ebuatsie@gmail.com>
2023-07-01 07:16:15 -0700

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value
+ * @r: input value
+ *
+ * Return: (0)
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r *= -1);
+	}
+}
