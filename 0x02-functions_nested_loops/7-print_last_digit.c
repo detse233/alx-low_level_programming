871f9cbec0fa59e2e143dabe48ca320c635297a2
Detse233 <ebuatsie@gmail.com>
2023-07-01 07:44:11 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit of number
+ *
+ * @r: input value
+ *
+ * Return: last digit of r
+ */
+int print_last_digit(int r)
+{
+	int i;
+
+	if (r < 0)
+	{
+		i = -1 * (r % 10);
+	}
+	else
+	{
+		i = r % 10;
+	}
+	_putchar((i % 10) + '0');
+	return (i % 10);
+}
