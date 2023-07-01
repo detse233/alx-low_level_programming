cda34912a7d488f0b4ac8267586ea694364e1dd0
Detse233 <ebuatsie@gmail.com>
2023-07-01 09:43:07 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two number
+ * @a: input value 1
+ * @b: input value 2
+ *
+ * Return: (0)
+ */
+int add(int a, int b)
+{
+	int result =  a + b;
+
+	return (result);
+}
