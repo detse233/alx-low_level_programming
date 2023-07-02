3febd9545651c3735989f54d7e33b468c7fa18cf
Detse233 <ebuatsie@gmail.com>
2023-07-02 08:20:08 -0700

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - combination of number
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i = '0';
+	int j;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '9')
+		{
+		if (i < j)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || i == '8' && j != '9')
+			putchar(',');
+			putchar(' ');
+		}
+		j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
