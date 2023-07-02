ab9657a54c22e767a4441e48c704d3b8fc8637cc
Detse233 <ebuatsie@gmail.com>
2023-07-02 07:36:47 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print base 16
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char n;
+	char t = 'a';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	while (t <= 'f')
+	{
+		putchar(t);
+		t++;
+	}
+	putchar('\n');
+}
