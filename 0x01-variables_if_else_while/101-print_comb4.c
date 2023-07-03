058db36670e4bb492988ac7498159a9c605b3b83
Detse233 <ebuatsie@gmail.com>
2023-07-03 09:16:43 -0700

    Print digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - print 3 digits
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i = '0';
+	int j;
+	int k;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '9')
+		{
+			k = '0';
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7')
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
